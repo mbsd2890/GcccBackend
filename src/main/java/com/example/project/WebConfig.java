7fc16d76865aed9c00cc5f96fc0bcd830aac7b40
mbsd2890 <mbsd2890@scs.ubbcluj.ro>
2024-06-16 15:04:20 +0300

    changed cors in web config

--- src/main/java/com/example/project/WebConfig.java
+++ src/main/java/com/example/project/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://wonderful-bay-04b3f7303.5.azurestaticapps.net", "http://localhost:4200")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
